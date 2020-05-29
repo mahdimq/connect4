6eb9358fa7c706d1664dd0a7dde67f6ae302a28d
Muhammad Qadir <mahdimq@gmail.com>
2020-05-29 20:12:11 +0300

    added logic and style

--- connect4.js
+++ connect4.js
@@ -4,6 +4,11 @@
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
  * board fills (tie)
  */
+const header = document.querySelector(".header")
+const info = document.querySelector(".info")
+const player = document.querySelector(".player-text")
+const token = document.querySelector(".token")
+const btn = document.querySelector('button')
 
 const WIDTH = 7;
 const HEIGHT = 6;
@@ -12,11 +17,14 @@ let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 
+player.classList.add('player-text')
+player.innerHTML = `Player ${currPlayer}'s turn`
+
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
-function makeBoard() {
+const makeBoard = () => {
   const grid = (x, y) => [ ...Array(x) ].map(val => Array(y).fill(null))
   // const board = new Array(HEIGHT).fill(null).map(() => new Array(WIDTH).fill(null));
   board = grid(WIDTH, HEIGHT)
@@ -36,7 +44,7 @@ function makeBoard() {
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
-function makeHtmlBoard() {
+const makeHtmlBoard = () => {
   // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
   const htmlBoard = document.getElementById("board")
 
@@ -59,6 +67,7 @@ function makeHtmlBoard() {
   // and have to use the for loop rather than forEach or For Of.
   for (let y = 0; y < HEIGHT; y++) { //<-- iterate through the board array to create columns
     const row = document.createElement("tr"); //<-- create row in the table
+    row.classList.add("row")
     for (let x = 0; x < WIDTH; x++) { //<-- iterate through the board array to create rows
       const cell = document.createElement("td"); //<-- create the table data cell
       cell.setAttribute("id", `${y}-${x}`); //<-- set id of cell to the index on the grid
@@ -70,7 +79,7 @@ function makeHtmlBoard() {
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
-function findSpotForCol(x) {
+const findSpotForCol = (x) => {
   // TODO: write the real version of this, rather than always returning 0
   for (let y = HEIGHT - 1; y > -1; y--) { //<-- Height - 1 = index of col
     if (!board[ y ][ x ]) {
@@ -83,29 +92,29 @@ function findSpotForCol(x) {
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
-function placeInTable(y, x) {
+const placeInTable = (y, x) => {
   // TODO: make a div and insert into correct table cell
   const piece = document.createElement("div")
   piece.classList.add("piece")
   piece.classList.add(`_${currPlayer}`)
-  // piece.style.top = -50 * (y + 2)
-  const spot = document.getElementById(`${y}-${x}`)
-  spot.append(piece)
+  const square = document.getElementById(`${y}-${x}`)
+  square.appendChild(piece)
 }
 
 /** endGame: announce game end */
 
-function endGame(msg) {
+const endGame = (msg) => {
   // TODO: pop up alert message
-  alert("Game over!") //<-- for now, until we get the winner/loser logic
+  alert(msg);
+  player.textContent = `Congratulations! Player ${currPlayer} won!` //<-- change header text
+  board = null
 }
 
 /** handleClick: handle click of column top to play piece */
 
-function handleClick(evt) {
+const handleClick = (evt) => {
   // get x from ID of clicked cell
   const x = +evt.target.id;
-  console.log("event target at x: ", x)
 
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
@@ -115,14 +124,12 @@ function handleClick(evt) {
 
   // place piece in board and add to HTML table
   // TODO: add line to update in-memory board
-  board[ y ][ x ] === currPlayer
+  board[ y ][ x ] = currPlayer
 
   placeInTable(y, x);
 
   // check for win
-  if (checkForWin()) {
-    return endGame(`Player ${currPlayer} won!`);
-  }
+  checkForWin() && endGame(`Congratulations! Player ${currPlayer} won!`);
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
@@ -131,15 +138,14 @@ function handleClick(evt) {
   // switch players
   // TODO: switch currPlayer 1 <-> 2
   currPlayer = currPlayer === 1 ? 2 : 1
-
-
-
+  currPlayer === 1 ? player.innerHTML = `Player ${currPlayer}'s turn` : player.innerHTML = `Player ${currPlayer}'s turn`
+  currPlayer === 1 ? token.style.color = "tomato" : token.style.color = "#289ee7"
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
 
-function checkForWin() {
-  function _win(cells) {
+const checkForWin = () => {
+  const _win = (cells) => {
     // Check four cells to see if they're all color of current player
     //  - cells: list of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
@@ -156,19 +162,24 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (let y = 0; y < HEIGHT; y++) {
-    for (let x = 0; x < WIDTH; x++) {
-      const horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
-      const vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
+  for (let y = 0; y < HEIGHT; y++) { //<-- loops over the columns
+    for (let x = 0; x < WIDTH; x++) { //<-- loops over the rows
+      const horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ]; //<-- calculates if rows and cols
+      const vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ]; // match up horz, vert, or diag
       const diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
       const diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
 
-      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-        return true;
+      if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) { //<-- conditional to check if tokens
+        return true;                                                   // line up horz, vert, or diagonally
       }
     }
   }
 }
+
+// Button to reset the game
+btn.addEventListener('click', () => {
+  window.location.reload()
+})
 
 makeBoard();
 makeHtmlBoard();
